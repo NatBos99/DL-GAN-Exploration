9042f7624dbf054ac1604e80faf87ee28bc7e28f
Nathan Bosch <nathangerbenbosch@gmail.com>
2021-05-22 15:42:25 +0200

    fix for GPU

--- GAN.py
+++ GAN.py
@@ -45,11 +45,11 @@ class GAN(pl.LightningModule):
         def compute_gradient_penalty(D, real_samples, fake_samples):
             """Calculates the gradient penalty loss for WGAN GP"""
             # Random weight term for interpolation between real and fake samples
-            alpha = torch.Tensor(np.random.random((real_samples.size(0), 1, 1, 1)))
+            alpha = Variable(torch.Tensor(np.random.random((real_samples.size(0), 1, 1, 1)))).to(self.device)
             # Get random interpolation between real and fake samples
             interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
             d_interpolates = D(interpolates)
-            fake = Variable(torch.Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
+            fake = Variable(torch.Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False).to(self.device)
             # Get gradient w.r.t. interpolates
             gradients = torch.autograd.grad(
                 outputs=d_interpolates,
